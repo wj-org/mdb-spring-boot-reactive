16ef7c12138185bc3ebe3179500839a15cbb8215
wenjie.teo <wenjie.teo@mongodb.com>
2023-12-26 20:17:40 +0800

    debit and credit to use transaction service

--- src/main/java/com/example/mdbspringbootreactive/controller/AccountController.java
+++ src/main/java/com/example/mdbspringbootreactive/controller/AccountController.java
@@ -36,65 +36,37 @@ public class AccountController {
 
     @PostMapping("/account/{accountNum}/debit")
     public Mono<ResponseMessage> debitAccount(@PathVariable String accountNum, @RequestBody Map<String,Object> requestBody){
+        Txn txn = new Txn();
         double amount = ((Number)requestBody.get("amount")).doubleValue();
-        return accountRepository.findAndIncrementBalanceByAccountNum(accountNum,amount).flatMap(updatedCount ->{
-            if(updatedCount<1){
-                throw new AccountNotFoundException();
-            }
-            return Mono.just(new ResponseMessage("success"));
-        });
+        txn.addEntry(new Txn.Entry(accountNum,amount));
+        return txnService.saveTransaction(txn)
+                .flatMap(txnService::executeTxn)
+                .then(Mono.just(new ResponseMessage("success")));
     }
 
     @PostMapping("/account/{accountNum}/credit")
     public Mono<ResponseMessage> creditAccount(@PathVariable String accountNum, @RequestBody Map<String,Object> requestBody){
+        Txn txn = new Txn();
         double amount = ((Number)requestBody.get("amount")).doubleValue();
-        return accountRepository.findAndIncrementBalanceByAccountNum(accountNum,-amount).flatMap(updatedCount ->{
-            if(updatedCount<1){
-                throw new AccountNotFoundException();
-            }
-            return Mono.just(new ResponseMessage("success"));
-        });
+        txn.addEntry(new Txn.Entry(accountNum,-amount));
+        return txnService.saveTransaction(txn)
+                .flatMap(txnService::executeTxn)
+                .then(Mono.just(new ResponseMessage("success")));
     }
 
-//    @PostMapping("/account/{accountNum}/debit")
-//    public Mono<Message> debitAccount(@PathVariable String accountNum, @RequestBody Map<String,Object> requestBody){
-//        Txn txn = new Txn();
-//        double amount = ((Number)requestBody.get("amount")).doubleValue();
-//        txn.addEntry(new Txn.Entry(accountNum,amount));
-//        return txnService.handleTransaction(txn);
-//    }
-//
-//    @PostMapping("/account/{accountNum}/credit")
-//    public Mono<Message> creditAccount(@PathVariable String accountNum, @RequestBody Map<String,Object> requestBody){
-//        Txn txn = new Txn();
-//        double amount = ((Number)requestBody.get("amount")).doubleValue();
-//        txn.addEntry(new Txn.Entry(accountNum,-amount));
-//        return txnService.handleTransaction(txn);
-//    }
 
-//    @Transactional
-//    @PostMapping("/account/{from}/transfer")
-//    public Mono<Message> transfer(@PathVariable String from, @RequestBody TransferRequest transferRequest){
-//        String to = transferRequest.getTo();
-//        double amount = ((Number)transferRequest.getAmount()).doubleValue();
-//        Txn txn = new Txn();
-//        txn.addEntry(new Txn.Entry(from,-amount));
-//        txn.addEntry(new Txn.Entry(to,amount));
-//        return txnService.handleTransaction(txn);
-//    }
-@PostMapping("/account/{from}/transfer")
-public Mono<ResponseMessage> transfer(@PathVariable String from, @RequestBody TransferRequest transferRequest){
-    String to = transferRequest.getTo();
-    double amount = ((Number)transferRequest.getAmount()).doubleValue();
-    Txn txn = new Txn();
-    txn.addEntry(new Txn.Entry(from,-amount));
-    txn.addEntry(new Txn.Entry(to,amount));
-    //save pending transaction then execute
-    return txnService.saveTransaction(txn)
-            .flatMap(txnService::executeTxn)
-            .then(Mono.just(new ResponseMessage("success")));
-}
-
+    @PostMapping("/account/{from}/transfer")
+    public Mono<ResponseMessage> transfer(@PathVariable String from, @RequestBody TransferRequest transferRequest){
+        String to = transferRequest.getTo();
+        double amount = ((Number)transferRequest.getAmount()).doubleValue();
+        Txn txn = new Txn();
+        txn.addEntry(new Txn.Entry(from,-amount));
+        txn.addEntry(new Txn.Entry(to,amount));
+        //save pending transaction then execute
+        return txnService.saveTransaction(txn)
+                .flatMap(txnService::executeTxn)
+                .then(Mono.just(new ResponseMessage("success")));
+    }
 
 
     @ExceptionHandler(AccountNotFoundException.class)
