913376fe0ef7531c90a9c3e7fb2e393fa0d39f03
wenjie.teo <wenjie.teo@mongodb.com>
2024-01-23 13:09:01 +0800

    remove unused imports use constructor injection instead of autowired remove paketo in pom extracted TxnEntry and TxnStatus

--- src/main/java/com/example/mdbspringbootreactive/model/Account.java
+++ src/main/java/com/example/mdbspringbootreactive/model/Account.java
@@ -1,6 +1,5 @@
 package com.example.mdbspringbootreactive.model;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("accounts")
